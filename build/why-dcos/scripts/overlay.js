b68b595774bf5f37aeffc41070b406ec8909e17f
Daniel Louwe Kooijmans <daniel@madeawkward.com>
2016-04-08 17:17:34 +0200

    Various tweaks

---
+++ build/why-dcos/scripts/overlay.js
@@ -0,0 +1,47 @@
+'use strict';
+
+(function () {
+
+  // grab data-toggle buttons
+  var btns = $$('[data-overlay-toggle');
+
+  // attach eventlisteners to the buttons
+  Array.prototype.map.call(btns, function (btn) {
+    btn.addEventListener('click', function (e) {
+      // grab reference to overlay based on data-attribute
+      var overlay = $('[data-overlay=' + btn.dataset.overlayToggle + ']');
+
+      // toggle the visibility CSS class
+      overlay.classList.toggle('overlay-visible');
+
+      // run open/close logic based on presence visibility class
+      var visible = overlay.classList.contains('overlay-visible');
+      if (visible) {
+        showOverlay.call(overlay);
+      } else {
+        closeOverlay.call(overlay);
+      }
+    });
+  });
+
+  // overlay is closed
+  function closeOverlay() {
+    this.classList.remove('overlay-visible');
+    document.removeEventListener('keydown', escapePressed);
+    document.removeEventListener('click', closeOverlay);
+  }
+
+  // overlay is shown
+  function showOverlay() {
+    // both esc or close button closes the overlay
+    document.addEventListener('keydown', escapePressed.bind(this));
+    this.querySelector('a.overlay-close').addEventListener('click', closeOverlay.bind(this));
+  }
+
+  function escapePressed(e) {
+    // if escape is pressed
+    if (e.keyCode === 27) {
+      closeOverlay.call(this);
+    }
+  }
+})();
