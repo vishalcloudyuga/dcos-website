d14ff315093dfa7c00ec2fabcbc8df095f06fd85
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2016-04-08 16:57:23 +0200

    Add quotes carousel

---
+++ src/scripts/quotes-carousel.js
@@ -0,0 +1,59 @@
+// logic for quote carousel
+(function() {
+  const slideSwitchTimeout = 5000;
+
+  let currentIndex = 0;
+  let quotes       = $$('.partner-quote-block');
+  let logos        = $$('.partner-quote-logo-list__partner-quote-logo');
+  let links        = $$('[data-activate-quote]');
+  let interval;
+
+  // setup listeners for logos
+  Array.prototype.map.call(links, (link) => {
+    link.addEventListener('click', logoClicked);
+  });
+
+  function logoClicked(e) {
+    e.preventDefault();
+    showQuoteWithIndex(this.dataset.activateQuote);
+    clearInterval(interval);
+  }
+
+  function showQuoteWithIndex(index) {
+    // hide current quote
+    let hidingQuote = quotes[currentIndex];
+    let hidingLogo  = logos[currentIndex];
+    hidingQuote.classList.remove('active');
+    hidingLogo.classList.remove('active');
+
+    setTimeout(() => {
+      hidingQuote.classList.add('hidden')
+
+      // select next quote
+      // new index passed?
+      if( index ) {
+        currentIndex = index
+      } else {
+        // nope; just increment
+        if ( currentIndex < quotes.length-1 ) {
+          currentIndex++
+        } else {
+          currentIndex = 0
+        }
+      }
+
+      // activate new quote
+      let showingQuote = quotes[currentIndex]
+      showingQuote.classList.remove('hidden')
+      showingQuote.classList.add('active')
+
+      // activate new logo
+      let showingLogo = logos[currentIndex]
+      showingLogo.classList.add('active')
+    }, 200);
+  }
+
+
+  interval = setInterval(showQuoteWithIndex, slideSwitchTimeout);
+
+})();
