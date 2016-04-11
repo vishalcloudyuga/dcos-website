02853d93f3d5b79836978ad1405e6dd4a4f1eaf4
David van Leeuwen <david@madeawkward.com>
2016-04-11 15:40:31 +0200

    Add header animation thingy

--- src/scripts/docs.js
+++ src/scripts/docs.js
@@ -1,73 +1,82 @@
 (function() {
-    let currentService, currentPlatform;
+  let currentService, currentPlatform;
 
-    const serviceLinks = $$('.service-select a');
-    const platformLinks = $$('.platform-select a');
+  const serviceLinks = $$('.service-select a');
+  const platformLinks = $$('.platform-select a');
 
-    let initialSetup = function (links, type) {
-      for (let i = links.length - 1; i >= 0; i--) {
-        links[i].addEventListener('click', function (e) {
-          e.preventDefault();
-          if(type === 'service') currentService = {name: e.currentTarget.getAttribute('data-name'), doc: e.currentTarget.getAttribute('data-doc')};
-          if(type === 'platform') currentPlatform = {name: e.currentTarget.getAttribute('data-name'), doc: e.currentTarget.getAttribute('data-doc')};
-          nextStep();
-        });
+  // Header animation
+  setInterval(function() {
+    let serviceLink = serviceLinks[Math.floor(Math.random()*serviceLinks.length)];
+    let platformLink = platformLinks[Math.floor(Math.random()*platformLinks.length)];
+    $('.getstarted-heading__service').innerHTML = serviceLink.getAttribute('data-name');
+    $('.getstarted-heading__platform').innerHTML = platformLink.getAttribute('data-name');
+  }, 1000)
+
+
+  // Steps with Markdown docs
+  let initialSetup = function (links, type) {
+    for (let i = links.length - 1; i >= 0; i--) {
+      links[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        if(type === 'service') currentService = {name: e.currentTarget.getAttribute('data-name'), doc: e.currentTarget.getAttribute('data-doc')};
+        if(type === 'platform') currentPlatform = {name: e.currentTarget.getAttribute('data-name'), doc: e.currentTarget.getAttribute('data-doc')};
+        nextStep();
+      });
+    }
+  }
+
+  initialSetup(serviceLinks, 'service');
+  initialSetup(platformLinks, 'platform');
+
+  let getDocs = function(servicePath, platformPath) {
+    return new Promise(function(resolve, reject) {
+      let serviceDoc, platformDoc;
+      let gotADoc = function () {
+        if(serviceDoc != undefined && platformDoc != undefined) resolve({service: serviceDoc, platform: platformDoc});
       }
+
+      fetch(`/docs/${servicePath}`)
+        .then(response => response.text())
+        .then(body => {
+          serviceDoc = body;
+          gotADoc()
+        })
+
+      fetch(`/docs/${platformPath}`)
+        .then(response => response.text())
+        .then(body => {
+          platformDoc = body;
+          gotADoc()
+        })
+    })
+  }
+
+  let nextStep = function () {
+    if(currentService != undefined && currentPlatform === undefined) {
+      // show platform
+      $('.service-select').setAttribute('style', 'display: none;');
+      $('.platform-select').removeAttribute('style');
+      $('.install').setAttribute('style', 'display: none;');
+
+      $('.step1').addClassName('step-inactive');
+      $('.step2').removeClassName('step-inactive');
+      $('.step1 h3').innerHTML = currentService.name;
+    } else if(currentService != undefined && currentPlatform != undefined) {
+      // show docs
+      $('.service-select').setAttribute('style', 'display: none;');
+      $('.platform-select').setAttribute('style', 'display: none;');
+      $('.install').removeAttribute('style');
+
+      $('.step2').addClassName('step-inactive');
+      $('.step3').removeClassName('step-inactive');
+      $('.step2 h3').innerHTML = currentPlatform.name;
+
+      getDocs(currentService.doc, currentPlatform.doc)
+        .then(docs => {
+          $('.install').innerHTML = `${docs.service}\n${docs.platform}`;
+        })
     }
-
-    initialSetup(serviceLinks, 'service');
-    initialSetup(platformLinks, 'platform');
-
-    let getDocs = function(servicePath, platformPath) {
-      return new Promise(function(resolve, reject) {
-        let serviceDoc, platformDoc;
-        let gotADoc = function () {
-          if(serviceDoc != undefined && platformDoc != undefined) resolve({service: serviceDoc, platform: platformDoc});
-        }
-
-        fetch(`/docs/${servicePath}`)
-          .then(response => response.text())
-          .then(body => {
-            serviceDoc = body;
-            gotADoc()
-          })
-
-        fetch(`/docs/${platformPath}`)
-          .then(response => response.text())
-          .then(body => {
-            platformDoc = body;
-            gotADoc()
-          })
-      })
-    }
-
-    let nextStep = function () {
-      if(currentService != undefined && currentPlatform === undefined) {
-        // show platform
-        $('.service-select').setAttribute('style', 'display: none;');
-        $('.platform-select').removeAttribute('style');
-        $('.install').setAttribute('style', 'display: none;');
-
-        $('.step1').addClassName('step-inactive');
-        $('.step2').removeClassName('step-inactive');
-        $('.step1 h3').innerHTML = currentService.name;
-      } else if(currentService != undefined && currentPlatform != undefined) {
-        // show docs
-        $('.service-select').setAttribute('style', 'display: none;');
-        $('.platform-select').setAttribute('style', 'display: none;');
-        $('.install').removeAttribute('style');
-
-        $('.step2').addClassName('step-inactive');
-        $('.step3').removeClassName('step-inactive');
-        $('.step2 h3').innerHTML = currentPlatform.name;
-
-        getDocs(currentService.doc, currentPlatform.doc)
-          .then(docs => {
-            $('.install').innerHTML = `${docs.service}\n${docs.platform}`;
-          })
-
-      }
-    }
+  }
 
 })();
 
