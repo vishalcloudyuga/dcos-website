db16dcec70ccd494ee7c4ab34c02742710d71eb5
David van Leeuwen <david@madeawkward.com>
2016-04-14 13:20:49 +0200

    Remove extension

--- src/scripts/docs.js
+++ src/scripts/docs.js
@@ -39,7 +39,6 @@
   Array.prototype.forEach.call($$('.docs-nav__item a[data-path]'), el => {
     el.addEventListener('click', function(e) {
       e.preventDefault();
-      console.log(e.target.parentNode)
       e.target.parentNode.hasClassName('docs-nav__item--closed') ? e.target.parentNode.removeClassName('docs-nav__item--closed') : e.target.parentNode.addClassName('docs-nav__item--closed')
     })
   })
