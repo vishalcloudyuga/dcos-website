bbe9d0de2f1d9a6af8be1a254612d6888f7610b5
David van Leeuwen <david@madeawkward.com>
2016-04-17 23:41:10 +0200

    Fix #385

--- src/scripts/stackdiagram.js
+++ src/scripts/stackdiagram.js
@@ -13,11 +13,7 @@
     if(window.scrollY+window.outerHeight >= $('.stack-diagram').offsetTop+450 && !isTriggered) {
       isTriggered = true;
 
-      let topTimer = setInterval(() => {
-        if(topElPos <= -50) clearInterval(topTimer);
-        topElPos -= 2;
-        topEl.style.top = `${topElPos}px`;
-      }, 15);
+      topEl.addClassName('animate');
 
       setTimeout(function() {
         $('.stack-diagram__top-info').removeClassName('hide');
@@ -25,19 +21,15 @@
         setTimeout(function() {
           $('.stack-diagram__middle-info').removeClassName('hide');
           setTimeout(function() {
-            let btmTimer = setInterval(() => {
-              if(btmElPos >= 80) clearInterval(btmTimer);
-              btmElPos += 2;
-              btmEl.style.top = `${btmElPos}px`;
-            }, 15);
+            btmEl.addClassName('animate');
 
             setTimeout(function() {
               $('.stack-diagram__bottom-info').removeClassName('hide');
-            }, 250)
+            }, 1000)
 
           }, 500)
-        }, 500)
-      }, 500)
+        }, 1000)
+      }, 750)
     }
   });
 
