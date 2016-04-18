073330a21bca248c74f8f4aa7d940dcc09b41fe9
David van Leeuwen <david@madeawkward.com>
2016-04-18 13:52:42 +0200

    Fix all zeh things

--- index.js
+++ index.js
@@ -147,13 +147,12 @@ Metalsmith(__dirname)
   .use(each(updatePaths))
   .clean(false)
   .use(uglify({
-    // order: ['scripts/main.js', 'scripts/**/*'],
     filter: 'scripts/**/*.js',
     concat: 'scripts/main.min.js',
-    // sourceMap: !process.env.CI,
-    // preserveComments: !process.env.CI,
-    // removeOriginal: process.env.CI,
-    // compress: process.env.CI
+    sourceMap: !process.env.CI,
+    preserveComments: !process.env.CI,
+    removeOriginal: process.env.CI,
+    compress: process.env.CI
   }))
   .use((() => {
     if(!process.env.CI) {
