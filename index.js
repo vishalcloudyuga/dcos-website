bc1e180c5c57ee60393b63a054f341c07adf24cc
David van Leeuwen <david@madeawkward.com>
2016-04-18 13:46:49 +0200

    *bump*

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ Metalsmith(__dirname)
   .use(uglify({
     // order: ['scripts/main.js', 'scripts/**/*'],
     filter: 'scripts/**/*.js',
-    // concat: 'scripts/main.min.js',
+    concat: 'scripts/main.min.js',
     // sourceMap: !process.env.CI,
     // preserveComments: !process.env.CI,
     // removeOriginal: process.env.CI,
