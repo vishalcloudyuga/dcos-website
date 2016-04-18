c1fb363dd58138d164ec2062a61bc9d8e7665537
Karl Isenberg <karl@mesosphere.io>
2016-04-18 12:47:57 -0700

    Add workflow steps

---
+++ ci/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd -P)"
+
+set -x
+
+cd "${REPO_ROOT}"
+
+declare -A branch_buckets
+branch_buckets[master]=s3://dcos.io
+branch_buckets[develop]=s3://dev.dcos.io
+
+current_branch="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+current_bucket="${branch_buckets["${current_branch}"]}"
+
+if [[ -z "${current_bucket}" ]]; then
+  echo "Unknown branch bucket (${current_branch}). Not deploying." >&2
+  exit 2
+fi
+
+s3cmd sync --delete-removed -r -P \
+  --access_key=$DCOSIO_S3_ACCESS_KEY \
+  --secret_key=$DCOSIO_S3_SECRET_KEY \
+  build/ ${branch_buckets["${current_branch}"]}
