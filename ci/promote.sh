8d0e3e8e5bfa3deddaa8760715b4330bcfb0e023
Karl Isenberg <karl@mesosphere.io>
2016-04-19 13:34:09 -0700

    fix promote script

--- ci/promote.sh
+++ ci/promote.sh
@@ -6,23 +6,29 @@ set -u
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd -P)"
 cd "${REPO_ROOT}"
 
-if git diff-index --quiet --cached HEAD; then
-  echo "Found staged changes - Exiting" >&2
-  exit 1
-fi
+echo "Fetching new code..."
+git fetch origin
 
-if git diff-files --quiet; then
+if ! git diff-files --quiet ; then
   echo "Found unstaged changes - Exiting" >&2
   exit 1
 fi
 
-if git ls-files --exclude-standard --others; then
+if ! git diff-index --quiet --cached HEAD ; then
+  echo "Found staged changes - Exiting" >&2
+  exit 1
+fi
+
+if ! git ls-files --exclude-standard --others ; then
   echo "Found untracked and unignored files - Exiting" >&2
   exit 1
 fi
 
-echo "Fetching new code..."
-git fetch origin
+if [[ -n "$(git status --porcelain -b)" ]]; then
+  echo "Found unpushed commits - Exiting" >&2
+  exit 1
+fi
+
 echo "Checking out master branch..."
 git checkout origin/master
 echo "Rebasing develop changes to master..."
