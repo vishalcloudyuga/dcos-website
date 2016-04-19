f805d8794cb469164ae7b8d4d5341c4945e5efa2
Karl Isenberg <karl@mesosphere.io>
2016-04-18 19:17:58 -0700

    guess-mime-type

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,4 +8,4 @@ cd "${REPO_ROOT}"
 
 current_bucket="$(ci/bucket.sh)"
 echo "Pushing to bucket: ${current_bucket}"
-s3cmd sync --delete-removed -r -P build/ "${current_bucket}"
+s3cmd sync --delete-removed --guess-mime-type --recursive --acl-public build/ "${current_bucket}"
