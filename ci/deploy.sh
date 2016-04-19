187ada26ff4b6ff547f524b1c4e66864fe9d933a
Karl Isenberg <karl@mesosphere.io>
2016-04-18 20:55:39 -0700

    switch to aws-cli from s3cmd

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,4 +8,4 @@ cd "${REPO_ROOT}"
 
 current_bucket="$(ci/bucket.sh)"
 echo "Pushing to bucket: ${current_bucket}"
-s3cmd sync --delete-removed --guess-mime-type --recursive --acl-public build/ "${current_bucket}"
+aws sync --delete --acl=public-read build/ "${current_bucket}"
