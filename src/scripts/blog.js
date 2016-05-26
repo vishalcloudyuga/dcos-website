d7e5499e63b6620899da5ecb7a94cb2461964e74
Timon van Spronsen <timonvanspronsen@outlook.com>
2016-05-26 12:22:37 +0200

    Equal height for posts and other improvements

--- src/scripts/blog.js
+++ src/scripts/blog.js
@@ -1,39 +1,70 @@
-const Blog = (function ($) {
+const Blog = (function($) {
+
   function init (containerEl) {
 
     // Initial params
     let offset = 7
     let limit = 6
 
-    let postsCache = []
-
     const $container = $(containerEl)
-    const $postsContainer = $container.find('.posts')
+    const postsContainer = $container.find('.posts')
     const $loadMoreButton = $('.load-more')
 
+    const loadMoreCallback = () => equalHeight(postsContainer)
+    const getPostsFromCache = getPosts()
+
+    equalHeight(postsContainer)
+
     $loadMoreButton.on('click', function () {
-      const $el = $(this)
-      $el.addClass('is-loading')
-
-      if (postsCache.length === 0) postsCache = getPosts()
-
-      postsCache.then(posts => {
-        $el.removeClass('is-loading')
-
-        if (posts.length < offset + limit) $el.remove()
-
-        posts
-          .slice(offset, (offset + limit))
-          .map(preparePostElement)
-          .map(addPostToDom($postsContainer))
-
-        offset += limit
-      })
+      loadMore.bind(this)(postsContainer, getPostsFromCache(), offset, limit, loadMoreCallback)
+        .then(() => offset += limit)
     })
   }
 
-  function getPosts (cache, offset, limit) {
-    return $.get('/posts/posts.json').then(data => data.result)
+  function equalHeight (container) {
+    const $container = $(container)
+    let maxHeight = 0
+
+    $container.children().each(function () {
+      const $el = $(this)
+      const height = $el.innerHeight()
+      if (height > maxHeight) maxHeight = height
+    })
+
+    $container.children().each(function () {
+      $(this).css('height', maxHeight)
+    })
+  }
+
+  function loadMore (container, posts, offset, limit, cb) {
+    const $el = $(this)
+    $el.addClass('is-loading')
+
+    return posts.then(_posts => {
+      $el.removeClass('is-loading')
+
+      if (_posts.length < (offset + limit)) $el.remove()
+
+      _posts
+        .slice(offset, (offset + limit))
+        .map(preparePostElement)
+        .map(addPostToDom(container))
+
+      if (typeof cb === 'function') cb(_posts)
+    })
+  }
+
+  function getPosts () {
+    let cache = []
+
+    return function () {
+      return cache.length > 0
+        ? $.when(cache)
+        : $.get('/posts/posts.json').then(data => {
+          cache = data.result
+          return $.when(cache)
+        })
+    }
   }
 
   function ellipsis (maxLength, text) {
@@ -45,7 +76,7 @@ const Blog = (function ($) {
   }
 
   function addPostToDom ($containerEl) {
-    return function (post) {
+    return function(post) {
       $containerEl.append(post)
     }
   }
@@ -53,6 +84,7 @@ const Blog = (function ($) {
   return {
     init
   }
+
 })(jQuery)
 
 window.Blog = Blog
