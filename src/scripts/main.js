0eb60c11df3a0255a9381ba17cf78a60de8e8b88
Davey Heuser <daveyheuser@gmail.com>
2016-11-17 21:00:40 +0100

    Fixed the button selection for 1 of the 3 columns

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1012,3 +1012,13 @@ if (!Array.prototype.find) {
 
     lib$es6$promise$polyfill$$default();
 }).call(this);
+
+
+/*******************************
+  Releases dropdown CTA
+*******************************/
+$( ".stable select" ).click(function() {
+  var currentValue = $( this ).val()
+  $('a[data-release]').addClass('hide')
+  $('a[data-release=' + currentValue + ']').removeClass('hide')
+});
