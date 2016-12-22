f72b1181b782fee6e8e5e30dba5e2f3a9c0068c6
Davey Heuser <daveyheuser@gmail.com>
2016-12-22 10:48:42 +0100

    changed project id

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -181,7 +181,7 @@ if($('.docs-nav__item__title.active').attr('href') == docPathName) {
 ****************/
 const docPageTitle = ($('h1.docs-heading').text()).replace(' ', '+')
 const jPid = 10101 // pid
-const jIssueType = 10100 // issuetype
+const jIssueType = 10201 // issuetype
 const jSummary = `Feedback+for+${docPageTitle}` // summary
 const jDescription = `Source: ${window.location.href}` // description
 
